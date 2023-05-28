b3f1a8f1650384dab1ad74b6d16c925e12bfadee
Tom Abbouz <tomabbouz@outlook.com>
2023-05-28 11:14:40 +0200

    Génération de l'arbre par Maven

---
+++ src/test/java/brownbelt/AppTest.java
@@ -0,0 +1,12 @@
+package brownbelt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
